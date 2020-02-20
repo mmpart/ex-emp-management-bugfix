6f9722c71a0d75a3304680ff2846e8a07d5e171d
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-02-20 18:19:34 +0900

    (1-3)Eメール重複チェックのコードを修正しました。

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -71,15 +70,16 @@ public class AdministratorController {
 	 */
 	@RequestMapping("/insert")
 	public String insert(@Validated InsertAdministratorForm form , BindingResult result,Model model) {
+		//入力値チェック
 		if(result.hasErrors()) {
 			return "administrator/insert";
 		}
 		
+		//Eメール重複チェック
 		Administrator administrator1 = administratorService.findByMailAddress(form.getMailAddress());
-		
 		if(administrator1 != null) {
 			String message = "存在するメールアドレスです";
-			model.addAttribute("message", message);
+			result.rejectValue("mailAddress", null, message);
 			return "administrator/insert";
 		}
 		Administrator administrator = new Administrator();
